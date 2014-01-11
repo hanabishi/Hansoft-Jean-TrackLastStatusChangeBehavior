681adb00224916fef325761d43a581f82173b83e
Svante Lidman <svante.lidman@gmail.com>
2014-01-11 11:05:34 +0100

    ProjectName parameter is now a regexp.
    The ProjectName parameter to the behavior is now a regular expression
    and the behavior will be applied for all projects with a name matching
    the regular expression.

--- TrackLastStatusChangeBehavior.cs
+++ TrackLastStatusChangeBehavior.cs
@@ -15,12 +15,14 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
     public class TrackLastStatusChangeBehavior : AbstractBehavior
     {
         string projectName;
+        bool initializationOK = false;
         string trackingColumnName;
         string trackedColumnName;
         string viewName;
         EHPMReportViewType viewType;
-        Project project;
-        private ProjectView projectView;
+        List<Project> projects;
+        private List<ProjectView> projectViews;
+        bool inverted = false;
         HPMProjectCustomColumnsColumn trackingColumn;
         HPMProjectCustomColumnsColumn trackedColumn;
         string title;
@@ -29,6 +31,9 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
             : base(configuration)
         {
             projectName = GetParameter("HansoftProject");
+            string invert = GetParameter("InvertedMatch");
+            if (invert != null)
+                inverted = invert.ToLower().Equals("yes");
             trackingColumnName = GetParameter("TrackingColumn");
             trackedColumnName = GetParameter("TrackedColumn");
             viewName = GetParameter("View");
@@ -38,21 +43,31 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
 
         public override void Initialize()
         {
-            project = HPMUtilities.FindProject(projectName);
-            if (project == null)
-                throw new ArgumentException("Could not find project:" + projectName);
-            if (viewType == EHPMReportViewType.AgileBacklog)
-                projectView = project.ProductBacklog;
-            else if (viewType == EHPMReportViewType.AllBugsInProject)
-                projectView = project.BugTracker;
-            else
-                projectView = project.Schedule;
-            trackedColumn = projectView.GetCustomColumn(trackedColumnName);
+            projects = new List<Project>();
+            projectViews = new List<ProjectView>();
+            initializationOK = false;
+            projects = HPMUtilities.FindProjects(projectName, inverted);
+            if (projects.Count == 0)
+                throw new ArgumentException("Could not find any matching project:" + projectName);
+            foreach (Project project in projects)
+            {
+                ProjectView projectView;
+                if (viewType == EHPMReportViewType.AgileBacklog)
+                    projectView = project.ProductBacklog;
+                else if (viewType == EHPMReportViewType.AllBugsInProject)
+                    projectView = project.BugTracker;
+                else
+                    projectView = project.Schedule;
+                projectViews.Add(projectView);
+            }
+
+            trackedColumn = projectViews[0].GetCustomColumn(trackedColumnName);
             if (trackedColumn == null)
                 throw new ArgumentException("Could not find custom column in view " + viewName + " " + trackedColumnName);
-            trackingColumn = projectView.GetCustomColumn(trackingColumnName);
+            trackingColumn = projectViews[0].GetCustomColumn(trackingColumnName);
             if (trackingColumn == null)
                 throw new ArgumentException("Could not find custom column in view " + viewName + " " + trackingColumnName);
+            initializationOK = true;
             DoUpdateFromHistory();
         }
 
@@ -81,8 +96,11 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
 
         private void DoUpdateFromHistory()
         {
-            foreach (Task task in projectView.DeepLeaves)
-                DoUpdateFromHistory(task);
+            foreach (ProjectView projectView in projectViews)
+            {
+                foreach (Task task in projectView.DeepLeaves)
+                    DoUpdateFromHistory(task);
+            }
         }
 
         private void DoUpdateFromHistory(Task task)
@@ -100,7 +118,9 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
 
         private void DoUpdateFromHistory(Task task, HPMDataHistory history)
         {
-            DateTimeValue storedValue =  (DateTimeValue)task.GetCustomColumnValue(trackingColumn);
+            // Ensure that we get the custom column of the right project
+            HPMProjectCustomColumnsColumn actualCustomColumn = task.ProjectView.GetCustomColumn(trackingColumn.m_Name);
+            DateTimeValue storedValue = (DateTimeValue)task.GetCustomColumnValue(actualCustomColumn);
             // ToInt64 will return the value as microseconds since 1970 Jan 1
             ulong storedHpmTime = storedValue.ToHpmDateTime();
             if (history.m_Latests.m_Time > storedHpmTime)
@@ -113,7 +133,7 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
                         if (entry.m_Time > storedHpmTime)
                         {
                             storedHpmTime = entry.m_Time;
-                            task.SetCustomColumnValue(trackingColumn, DateTimeValue.FromHpmDateTime(task, trackingColumn, storedHpmTime));
+                            task.SetCustomColumnValue(trackingColumn, DateTimeValue.FromHpmDateTime(task, actualCustomColumn, storedHpmTime));
                         }
                     }
                 }
@@ -122,20 +142,26 @@ namespace Hansoft.Jean.Behavior.TrackLastStatusChangeBehavior
 
         public override void OnTaskChangeCustomColumnData(TaskChangeCustomColumnDataEventArgs e)
         {
-            if (e.Data.m_ColumnHash == trackedColumn.m_Hash)
+            if (initializationOK)
             {
-                Task task = Task.GetTask(e.Data.m_TaskID);
-                if (task.MainProjectID.m_ID == project.UniqueID.m_ID && task.ProjectView.Equals(projectView))
-                    task.SetCustomColumnValue(trackingColumn, DateTimeValue.FromHpmDateTime(task, trackingColumn, HPMUtilities.HPMNow()));
+                if (e.Data.m_ColumnHash == trackedColumn.m_Hash)
+                {
+                    Task task = Task.GetTask(e.Data.m_TaskID);
+                    if (projects.Contains(task.Project) && projectViews.Contains(task.ProjectView))
+                        task.SetCustomColumnValue(trackingColumn, DateTimeValue.FromHpmDateTime(task, trackingColumn, HPMUtilities.HPMNow()));
+                }
             }
         }
 
         public override void OnDataHistoryReceived(DataHistoryReceivedEventArgs e)
         {
-            if (SessionManager.Session.UtilIsIDTask(e.Data.m_UniqueIdentifier) || SessionManager.Session.UtilIsIDTaskRef(e.Data.m_UniqueIdentifier))
+            if (initializationOK)
             {
-                Task task = Task.GetTask(e.Data.m_UniqueIdentifier);
-                DoUpdateFromHistory(task);
+                if (SessionManager.Session.UtilIsIDTask(e.Data.m_UniqueIdentifier) || SessionManager.Session.UtilIsIDTaskRef(e.Data.m_UniqueIdentifier))
+                {
+                    Task task = Task.GetTask(e.Data.m_UniqueIdentifier);
+                    DoUpdateFromHistory(task);
+                }
             }
         }
     }
